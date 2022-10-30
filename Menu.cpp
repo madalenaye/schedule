cf2d9dce8cbb88c79d44a8588074a5241e871bb4
José Santos <sjse7780@gmail.com>
2022-10-30 03:03:10 +0000

    Fixed Slot

--- Menu.cpp
+++ Menu.cpp
@@ -2,4 +2,17 @@
 // Created by Madalena Ye on 29/10/2022.
 //
 
+#include <iostream>
+#include <iomanip>
 #include "Menu.h"
+using namespace std;
+void createMenu() {
+    printf("\033[44m==================================================== IɴғᴏPᴏᴄᴋᴇᴛ ================================================================\033[0m\t\t");
+    printf("\n");
+    printf("\n");
+    cout << setw(79) << "Welcome to the best schedule manager! \n" << setw(74) << "What would you like to do today?"
+         << endl;
+    printf("\n");
+    printf("\033[45m================================================================================================================================\033[0m\t\t");
+
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "ReadFiles.h"
 #include <iostream>
 #include <iomanip>
+#include "Menu.h"
 using namespace std;
 int main() {
 
     readClasses();
     readStudentClasses();
     readClassesPerUC();
-
+    createMenu();
 }
