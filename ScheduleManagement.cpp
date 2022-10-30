e1a0d04cad6c68662251c52304f439eccc684fe3
Madalena Ye <mads1040@gmail.com>
2022-10-30 01:10:02 +0100

    created scheduleManagement class and planning to create a menu

---
+++ ScheduleManagement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Madalena Ye on 30/10/2022.
+//
+
+#include "ScheduleManagement.h"
---
+++ ScheduleManagement.h
@@ -0,0 +1,28 @@
+//
+// Created by Madalena Ye on 30/10/2022.
+//
+
+#ifndef SCHEDULE_SCHEDULEMANAGEMENT_H
+#define SCHEDULE_SCHEDULEMANAGEMENT_H
+
+#include "StudentClasses.h"
+#include "Lecture.h"
+#include <set>
+#include <vector>
+class ScheduleManagement{
+public:
+    //constructors
+    ScheduleManagement();
+    ScheduleManagement(set<StudentClasses> students, vector<Lecture> schedule);
+    //getters
+    set<StudentClasses> get_students();
+    vector<Lecture> get_schedule();
+    //setters
+    void set_students(set<StudentClasses> stu);
+    void set_schedule(vector<Lecture> sch);
+private:
+    set<StudentClasses> students;
+    vector<Lecture> schedule;
+
+};
+#endif //SCHEDULE_SCHEDULEMANAGEMENT_H
