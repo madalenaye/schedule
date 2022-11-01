62f6eb8a791872962a42915fe74809f9300e0ec1
Madalena Ye <mads1040@gmail.com>
2022-11-01 03:07:40 +0000

    updating menu

--- Menu.cpp
+++ Menu.cpp
@@ -5,16 +5,33 @@
 #include <iostream>
 #include <iomanip>
 #include "Menu.h"
+#include <unistd.h>
 using namespace std;
 
 void createMenu(){
+    printf("\n");
     printf("\033[44m======================== IɴғᴏPᴏᴄᴋᴇᴛ =========================\033[0m\t\t");
     printf("\n");
     printf("\n");
-    cout << setw(52) << "Welcome to the best schedule manager! \n" << setw(54) << "What operation would you like to perform today?"
+    cout << "Bem-vind@ ao melhor gestor de horários! \n"<< "O que deseja fazer hoje?"
          << endl;
     printf("\n");
-    cout << setw(36) << "1. Listings\n" << setw(38) <<"2. Modifications\n" << setw(33) << "3. Exit\n";
+    cout << "1. Listagens\n" << "2. Alterações\n" << "3. Sair do programa\n";
+
+    string option;
+    cin >> option;
+    while (!(option == "1" || option == "2" || option == "3")){
+        cout << "Input inválido, tente novamente: ";
+        cin >> option;
+    }
+    if (option == "1"){
+        cout << "Selecione o tipo de listagem:\n";
+        cout << "1. Listagem de turmas\n" << "2. Listagem de alunos\n" << "3. Listagem de unidades curriculares\n" << "4. Listagem de horário\n" << "5. Voltar\n";
+    }
+    else if (option == "2"){
+
+    }
+    else endMenu();
 }
 
 void endMenu(){
