e3922f611f2857d697797332f2361d1a6a574663
unknown <joaofigueiredo2003@gmail.com>
2022-11-03 15:05:17 +0000

    Main

--- main.cpp
+++ main.cpp
@@ -5,14 +5,31 @@
 #include "Menu.h"
 
 int main(){
-    createMenu();
-    /*
+    //createMenu();
+
     ScheduleManagement manager;
     manager.readStudents();
     manager.listingStudentsInClass("L.EIC002","1LEIC08");
-    cout<<"====================================="<<endl;
+    cout<<manager.studentsPerClass("L.EIC002","1LEIC08")<<endl;
+    Request r1(REMOVE,202025487,"L.EIC002","1LEIC08","");
+    Request r2 (ADD,202025487,"L.EIC002","1LEIC08","");
+    Request r3(CHANGE_CLASS,202023532,"L.EIC002","1LEIC02","1LEIC08");
+    manager.push_request(r1);
+    manager.push_request(r2);
+    manager.push_request(r3);
+    manager.doRequest();
+    cout<<manager.studentsPerClass("L.EIC002","1LEIC08")<<endl;
     manager.listingStudentsInClass("L.EIC002","1LEIC08");
-     */
+    manager.doRequest();
+    cout<<manager.studentsPerClass("L.EIC002","1LEIC08")<<endl;
+    manager.listingStudentsInClass("L.EIC002","1LEIC08");
+    manager.doRequest();
+    cout<<manager.studentsPerClass("L.EIC002","1LEIC08")<<endl;
+    manager.listingStudentsInClass("L.EIC002","1LEIC08");
+
+    //manager.removeStudent(202025487,"L.EIC002","1LEIC08");
+    //cout<<"====================================="<<endl;
+    //manager.listingStudentsInClass("L.EIC002","1LEIC08");
     //manager.listingClassPerYear((char)year[0]);
     //manager.listingClassSchedule("1LEIC01");
     //manager.listingStudentSchedule("202071557");
@@ -20,5 +37,5 @@ int main(){
     /*
     manager.listingClasses("1");*/
 
-    endMenu();
+    //endMenu();
 }
